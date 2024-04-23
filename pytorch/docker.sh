b302c23ada8f9d7f733e33dd23272291a1276e23
tf63 <tfukuda9031@gmail.com>
2024-04-24 06:26:29 +0900

    impl torch

---
+++ pytorch/docker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# https://github.com/RUB-SysSec/GANDCTAnalysis/blob/master/docker.sh
+
+# usage ----------------------------------------------
+# bash docker.sh build  # build image
+# bash docker.sh shell  # run container as user
+# bash docker.sh root  # run container as root
+# ----------------------------------------------------
+
+DATASET_DIRS="$HOME/dataset"
+DATA_DIRS="$HOME/data"
+TORCH_VERSION="torch-1.9.0"
+DOCKERFILE_NAME="Dockerfile.cu111"
+
+build()
+{
+    docker build . -f docker/$DOCKERFILE_NAME --target $TORCH_TAG --build-arg USER_UID=`(id -u)` --build-arg USER_GID=`(id -g)` -t $TORCH_TAG
+}
+
+shell() 
+{
+    docker run --rm --gpus all --shm-size=16g -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_TAG /bin/bash
+}
+
+root()
+{
+    docker run --rm --gpus all --shm-size=16g --user 0:0 -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_TAG /bin/bash
+}
+
+help()
+{
+    echo "usage: bash docker.sh [build|shell|root|help]"
+}
+
+
+if [[ $1 == "build" ]]; then
+    build $2
+elif [[ $1 == "shell" ]]; then
+    shell 
+elif [[ $1 == "root" ]]; then
+    root
+elif [[ $1 == "help" ]]; then
+    help
+else
+    help
+fi
