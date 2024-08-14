1f8ef4760a607766558784a8506a936a481b34cc
tf63 <74246282+tf63@users.noreply.github.com>
2024-08-14 17:03:59 +0900

    feat: update poetry-poetry environment

--- pytorch-poetry/README.md
+++ pytorch-poetry/README.md
@@ -1,15 +1,31 @@
-# PyTorch + poetry環境
+# PyTorch + Poetry環境
+> [!CAUTION]
+> `docker.sh`のTORCH_VERSIONはただのタグ
+> 切り替えて自動でバージョンが変わるわけでは無い
 
-初回に`poetry install`してプロジェクト直下に仮想環境`.venv/`を作成する
-仮想環境を使うには`poetry run python3`する必要があることに注意
 
-## 環境をカスタムする場合
-`poetry add`でtorchを入れて`pyproject.toml`と`poetry.lock`を作る作戦
+torchのバージョンを切り替えたい場合は`pyproject.toml`をいじる
+```pyproject.toml
+[tool.poetry.dependencies]
+python = "^3.8"
+# ↓
+torch = "1.9.0+cu111"
 ```
-    # + -> %2B としないと弾かれます
 
-    # https://download.pytorch.org/whl/cu111/torch/
-    poetry add https://download.pytorch.org/whl/cu111/torch-1.9.0%2Bcu111-cp38-cp38-linux_x86_64.whl
-    # https://download.pytorch.org/whl/cu111/torchvision
-    poetry add https://download.pytorch.org/whl/cu111/torchvision-0.10.0%2Bcu111-cp38-cp38-linux_x86_64.whl
+lockファイルと`pyproject.toml`の一貫性がなくなり，イメージのビルドが失敗するはず
+
+その場合，`Dockerfile`から`RUN poetry install`をコメントアウトしてビルドする
+```Dockerfile
+FROM base AS torch-1.9.0
+
+COPY --chown=$USER_NAME:$GROUP_NAME pyproject.toml poetry.lock ./
+# ↓
+RUN poetry install
 ```
+
+コンテナ内で次のコマンドを実行し，lockファイルを更新する
+```shell
+poetry lock
+```
+
+`RUN poetry install`のコメントを外してビルドしなおせば上手くいく
--- pytorch-poetry/docker.sh
+++ pytorch-poetry/docker.sh
@@ -1,47 +1,92 @@
 #!/bin/bash
-# https://github.com/RUB-SysSec/GANDCTAnalysis/blob/master/docker.sh
 
 # usage ----------------------------------------------
 # bash docker.sh build  # build image
+# bash docker.sh build:no-cache  # build image (no-cache)
 # bash docker.sh shell  # run container as user
-# bash docker.sh root  # run container as root
+# bash docker.sh shell:root  # run container as root
 # ----------------------------------------------------
 
 DATASET_DIRS="$HOME/dataset"
 DATA_DIRS="$HOME/data"
 
-EXP_NAME="sample"
+EXP_NAME="poetry"
 DOCKERFILE_NAME="Dockerfile.cu111"
 TORCH_VERSION="torch-1.9.0"
+# /app/.venvをvolumeに退避することで，/appをPWDでマウントしても.venvが残るようにする
+VOLUME_NAME=$EXP_NAME-$TORCH_VERSION
 # cu111 - torch-1.9.0
 # cu117 - torch-1.13.0, torch-2.0.0
 
 build()
 {
-    DOCKER_BUILDKIT=1 docker build . -f docker/$DOCKERFILE_NAME --target $TORCH_VERSION --build-arg USER_UID=`(id -u)` --build-arg USER_GID=`(id -g)` -t $TORCH_VERSION:$EXP_NAME
+    docker volume inspect $VOLUME_NAME > /dev/null 2>&1 || docker volume create $VOLUME_NAME
+    DOCKER_BUILDKIT=1 docker build . -f docker/$DOCKERFILE_NAME \
+                                    --target $TORCH_VERSION \
+                                    --build-arg USER_UID=`(id -u)` \
+                                    --build-arg USER_GID=`(id -g)` \
+                                    -t $TORCH_VERSION:$EXP_NAME
+}
+
+build:no-cache()
+{
+    volume
+    DOCKER_BUILDKIT=1 docker build . -f docker/$DOCKERFILE_NAME \
+                                    --no-cache \
+                                    --target $TORCH_VERSION \
+                                    --build-arg USER_UID=`(id -u)` \
+                                    --build-arg USER_GID=`(id -g)` \
+                                    -t $TORCH_VERSION:$EXP_NAME
 }
 
 shell()
 {
-    docker run --name $EXP_NAME --rm --gpus all --shm-size=32g -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data -v $TORCH_VERSION:$EXP_NAME /bin/bash
+    docker run --name $EXP_NAME \
+                --rm -it \
+                --gpus all \
+                --shm-size=32g \
+                -v $(pwd):/app \
+                -v $DATASET_DIRS:/dataset \
+                -v $DATA_DIRS:/data \
+                -v $VOLUME_NAME:/app/.venv \
+                $TORCH_VERSION:$EXP_NAME /bin/bash
 }
 
-root()
+shell:root()
 {
-    docker run --name $EXP_NAME --rm --gpus all --shm-size=32g --user 0:0 -it -v $(pwd):/app -v $DATASET_DIRS:/dataset -v $DATA_DIRS:/data $TORCH_VERSION:$EXP_NAME /bin/bash
+    docker run --name $EXP_NAME \
+                --user 0:0 \
+                --rm -it \
+                --gpus all \
+                --shm-size=32g \
+                -v $(pwd):/app \
+                -v $DATASET_DIRS:/dataset \
+                -v $DATA_DIRS:/data \
+                -v $VOLUME_NAME:/app/.venv \
+                $TORCH_VERSION:$EXP_NAME /bin/bash
+}
+
+volume()
+{
+    docker volume inspect $VOLUME_NAME > /dev/null 2>&1 && echo "remove volume" && docker volume rm $VOLUME_NAME
+    echo "create volume" && docker volume create $VOLUME_NAME
 }
 
 help()
 {
-    echo "usage: bash docker.sh [build|shell|root|help]"
+    echo "usage: bash docker.sh [build|build:no-cache|shell|shell:root|volume|help]"
 }
 
 if [[ $1 == "build" ]]; then
-    build $2
+    build
+elif [[ $1 == "build:no-cache" ]]; then
+    build:no-cache
 elif [[ $1 == "shell" ]]; then
     shell
-elif [[ $1 == "root" ]]; then
-    root
+elif [[ $1 == "shell:root" ]]; then
+    shell:root
+elif [[ $1 == "volume" ]]; then
+    volume
 elif [[ $1 == "help" ]]; then
     help
 else
