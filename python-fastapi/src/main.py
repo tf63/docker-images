070231008e41a414dd97548026a696e986a19091
tf63 <74246282+tf63@users.noreply.github.com>
2024-08-18 11:31:14 +0900

    feat: update fastapi environment

--- python-fastapi/src/main.py
+++ python-fastapi/src/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def read_root():
-    return {"Hello": "World"}
+    return {'Hello': 'World'}
 
 
-@app.get("/items/{item_id}")
+@app.get('/items/{item_id}')
 def read_item(item_id: int, q: str):
-    return {"item_id": item_id, "q": q}
+    return {'item_id': item_id, 'q': q}
